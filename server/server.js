865c88141f192ac17de4a7e65e237d436b1e5a95
Kykesh <150619538+Kykesh@users.noreply.github.com>
2024-05-12 13:27:36 -0500

    Update Server.js
    update path to deployment page to fix render blank page

--- server/server.js
+++ server/server.js
@@ -22,12 +22,12 @@ const startApolloServer = async () => {
   app.use(express.json());
 
   if (process.env.NODE_ENV === 'production') {
-    app.use('/static', express.static(path.join(__dirname, '../client/src/static')));
+    app.use(express.static(path.join(__dirname, '../client/build')));
     app.get('*', (req, res) => {
-      res.sendFile(path.join(__dirname, '../client/index.html'));
+      res.sendFile(path.join(__dirname, '../client/build/index.html'));
     });
   }
-
+  
   app.use(server.getMiddleware({ path: '/graphql' }));
 
   db.once('open', () => {
